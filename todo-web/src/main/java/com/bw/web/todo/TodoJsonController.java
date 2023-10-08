289c61110a5f2b33b25a3402ec41b8551aca1ca5
Brantley Wells <brantleywells@gmail.com>
2023-10-07 22:45:58 -0400

    Forgot a file.

--- todo-web/src/main/java/com/bw/web/todo/TodoJsonController.java
+++ todo-web/src/main/java/com/bw/web/todo/TodoJsonController.java
@@ -3,6 +3,7 @@ package com.bw.web.todo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -27,7 +28,7 @@ import com.bw.web.todo.security.TodoUser;
 @RequestMapping("/rest")
 public class TodoJsonController {
 	
-	private static Logger logger = Logger.getLogger(TodoJsonController.class);
+	private static Logger logger = LogManager.getLogger(TodoJsonController.class);
 	@Autowired
 	private TodoDao todoDao;
 	
